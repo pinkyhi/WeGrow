cbcbd159ae4a46265f162249529ba212dbf5761e
Ivan Vladymyrov <ivanvladymyrov.pinky@gmail.com>
2022-04-07 16:54:26 +0300

    DbContext initialization

--- WeGrow/Program.cs
+++ WeGrow/Program.cs
@@ -1,6 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
+using WeGrow.DAL;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+ 
+var app = builder.Build();
 
 app.Run();
