20d1b3cf1f5cebf3856448f5b5a54d1d8d9e9183
Ivan Vladymyrov <ivanvladymyrov.pinky@gmail.com>
2022-04-16 16:32:13 +0300

    read admin table

--- WeGrow/Models/Entities/ModuleEntity.cs
+++ WeGrow/Models/Entities/ModuleEntity.cs
@@ -7,10 +7,10 @@ namespace WeGrow.Models.Entities
     [AutoMap(typeof(Module), ReverseMap = true)]
     public class ModuleEntity
     {
-        public int Id { get; set; }
+        public int Id { get; private set; }
         public ModuleType Type { get; set; }
         public ModuleSubject Subject { get; set; }
-        public string Title { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public int Amount { get; set; }
