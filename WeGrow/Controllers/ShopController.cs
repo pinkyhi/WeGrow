695ce92ba63de5d083091298f369284ab3e09b0b
Ivan Vladymyrov <ivanvladymyrov.pinky@gmail.com>
2022-04-23 21:42:35 +0300

    shop controller

---
+++ WeGrow/Controllers/ShopController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using WeGrow.DAL.Interfaces;
+using WeGrow.Models.Entities;
+
+namespace WeGrow.Controllers
+{
+    [Route("shop")]
+    public class ShopController : Controller
+    {
+        private readonly IRepository repository;
+        private readonly IMapper mapper;
+
+        public ShopController(IRepository repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        [Route("modules")]
+        [HttpGet]
+        public async Task<IActionResult> GetModules()
+        {
+            var items = await repository.GetRangeAsync<Module>(false, x => true);
+            var itemsEntities = items.Select(x => mapper.Map<ModuleEntity>(x));
+            return Ok(itemsEntities);
+        }
+    }
+}
