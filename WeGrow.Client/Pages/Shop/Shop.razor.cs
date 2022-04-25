3e028c0aff4e3c47ab7c1e466128abb0b71fd189
Ivan Vladymyrov <ivanvladymyrov.pinky@gmail.com>
2022-04-26 00:59:17 +0300

    shop filter model mapping

--- WeGrow.Client/Pages/Shop/Shop.razor.cs
+++ WeGrow.Client/Pages/Shop/Shop.razor.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using WeGrow.Core.Helpers;
 using WeGrow.Core.Resources;
 using WeGrow.Models.Entities;
 using WeGrow.Models.Shop;
@@ -21,6 +23,9 @@ namespace WeGrow.Client.Pages.Shop
 
         public string ApiUrl { get; set; }
 
+        private bool isLoading = false;
+
+        [Inject] private HttpClient HttpClient { get; set; }
         [Inject] private IConfiguration Configuration { get; set; }
 
         protected override void OnInitialized()
@@ -30,7 +35,38 @@ namespace WeGrow.Client.Pages.Shop
 
         protected async Task OnFilterApplied(ModulesShopFilter filterModel)
         {
-            await Task.Delay(2000);
+            
+            var queryParams = new Dictionary<string, string>();
+
+            if (CurrentPage != null)
+            {
+                queryParams.Add("page", CurrentPage.ToString());
+            }
+            if (!string.IsNullOrWhiteSpace(Search))
+            {
+                queryParams.Add("search", Search);
+            }
+            foreach(var param in QueryMapHelper.GetDictionaryFromModel(filterModel))
+            {
+                queryParams.Add(param.Key, param.Value);
+            }
+
+            string url = QueryHelpers.AddQueryString(ApiUrl, queryParams);
+
+            isLoading = true;
+
+            var result = await HttpClient.GetAsync(url);
+
+            if (result.IsSuccessStatusCode)
+            {
+                ItemsList = await result.Content.ReadFromJsonAsync<List<ModuleEntity>>();
+                isLoading = false;
+            }
+            else
+            {
+                isLoading = false;
+                throw new Exception("Fetch data error");
+            }
         }
     }
 }
---
+++ WeGrow.Core/Helpers/QueryMapHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WeGrow.Core.Helpers
+{
+    public static class QueryMapHelper
+    {
+        public static Dictionary<string, string> GetDictionaryFromModel<T>(T model) where T : new()
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            var modelType = typeof(T);
+            var modelProperties = modelType.GetProperties();
+            foreach(var property in modelProperties)
+            {
+                var value = property.GetValue(model);
+                if (value != null)
+                {
+                    var valueType = property.PropertyType;
+                    if (valueType != typeof(string) && (valueType.GetInterfaces().Contains(typeof(IEnumerable)) || valueType is IEnumerable))
+                    {
+                        var enumerableValue = value as IEnumerable<object>;
+                        string joinedArr = string.Join(',', enumerableValue);
+                        result.Add(property.Name, joinedArr);
+                    }
+                    else
+                    {
+                        result.Add(property.Name, value.ToString());
+                    }
+                }
+            }
+
+            return result;
+        }
+        public static T GetModelFromQueryUrl<T>(string url) where T : new()
+        {
+            Uri uri = new Uri(url);
+            var keyValues = HttpUtility.ParseQueryString(uri.Query);
+            return GetModelFromDictionary<T>(NameValuesToDictionary(keyValues));
+        }
+
+        public static T GetModelFromDictionary<T>(Dictionary<string, string> valuesDict) where T : new()
+        {
+            T result = new T();
+            var modelType = typeof(T);
+            var modelProperties = modelType.GetProperties();
+            foreach(var property in modelProperties)
+            {
+                var propVal = valuesDict.FirstOrDefault(x => string.Equals(property.Name, x.Key, StringComparison.OrdinalIgnoreCase)).Value;
+                if (property.PropertyType?.GetInterfaces().Contains(typeof(IEnumerable<>)) == true && string.IsNullOrWhiteSpace(propVal))
+                {
+                    var splittedArr = propVal.Split(',').ToList();
+                    property.SetValue(result, propVal);
+                }
+                else if (string.IsNullOrWhiteSpace(propVal))
+                {
+                    property.SetValue(result, propVal);
+                }
+            }
+            return result;
+        }
+
+        public static Dictionary<string, string> NameValuesToDictionary(NameValueCollection nameValues)
+        {
+            var dict = new Dictionary<string, string>();
+
+            if (nameValues != null)
+            {
+                foreach (string key in nameValues.AllKeys)
+                {
+                    dict.Add(key, nameValues[key]);
+                }
+            }
+
+            return dict;
+        }
+    }
+}
