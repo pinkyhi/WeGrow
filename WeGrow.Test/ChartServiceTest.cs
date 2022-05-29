7a002144c25467cd3f531473d223b8745469e3da
Ivan Vladymyrov <ivanvladymyrov.pinky@gmail.com>
2022-05-29 19:09:18 +0300

    chart tests

---
+++ WeGrow.Test/ChartServiceTest.cs
@@ -0,0 +1,52 @@
+using Moq;
+using System.Collections.Generic;
+using WeGrow.Client.Services;
+using Xunit;
+
+namespace WeGrow.Test
+{
+    public class ChartServiceTest
+    {
+        [Fact]
+        public void CreateExtendedChartTest()
+        {
+            var chartService = new ChartService();
+
+            var sourceChart = new Dictionary<int, double>();
+            sourceChart.Add(0, 10);
+            sourceChart.Add(24, 10);
+
+            chartService.CreateExtendedChart(sourceChart, out Dictionary<int, double> destChart);
+
+            Assert.Equal(25, destChart.Count);
+        }
+
+        [Fact]
+        public void GetExactValue()
+        {
+            var chartService = new ChartService();
+
+            var sourceChart = new Dictionary<int, double>();
+            sourceChart.Add(0, 10);
+            sourceChart.Add(24, 10);
+
+            chartService.CreateExtendedChart(sourceChart, out Dictionary<int, double> destChart);
+            
+            Assert.Equal(10, chartService.GetExactValue(destChart, 12.5));
+        }
+
+        [Fact]
+        public void GetDayChart()
+        {
+            var chartService = new ChartService();
+
+            var sourceChart = new Dictionary<int, double>();
+            sourceChart.Add(0, 10);
+            sourceChart.Add(24, 10);
+            chartService.CreateExtendedChart(sourceChart, out Dictionary<int, double> destChart);
+            var dayChart = chartService.GetDayChart(destChart);
+
+            Assert.Equal(25, dayChart.Count);
+        }
+    }
+}
---
+++ WeGrow.Test/ShopTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeGrow.Client.Services;
+using Xunit;
+
+namespace WeGrow.Test
+{
+    public class ShopTest
+    {
+        [Fact]
+        public void CreateExtendedChartTest()
+        {
+            var chartService = new ChartService();
+
+            var sourceChart = new Dictionary<int, double>();
+            sourceChart.Add(0, 10);
+            sourceChart.Add(24, 10);
+
+            chartService.CreateExtendedChart(sourceChart, out Dictionary<int, double> destChart);
+
+            Assert.Equal(25, destChart.Count);
+        }
+
+        [Fact]
+        public void GetExactValue()
+        {
+            var chartService = new ChartService();
+
+            var sourceChart = new Dictionary<int, double>();
+            sourceChart.Add(0, 10);
+            sourceChart.Add(24, 10);
+
+            chartService.CreateExtendedChart(sourceChart, out Dictionary<int, double> destChart);
+
+            Assert.Equal(10, chartService.GetExactValue(destChart, 12.5));
+        }
+
+        [Fact]
+        public void GetDayChart()
+        {
+            var chartService = new ChartService();
+
+            var sourceChart = new Dictionary<int, double>();
+            sourceChart.Add(0, 10);
+            sourceChart.Add(24, 10);
+            chartService.CreateExtendedChart(sourceChart, out Dictionary<int, double> destChart);
+            var dayChart = chartService.GetDayChart(destChart);
+
+            Assert.Equal(25, dayChart.Count);
+        }
+    }
+}
